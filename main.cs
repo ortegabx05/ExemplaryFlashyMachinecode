fa9514a95faf47c9a2845c021c6856deb94d6b50
ortegabx05 <30540228+ortegabx05@users.noreply.github.com>
2023-09-07 14:23:01 +0000

    Changed code to single display

--- main.cs
+++ main.cs
@@ -3,52 +3,16 @@ using System;
 class PersonalInfo {
   public static void Main (string[] args) {
 
-    string name1= "Lucinda Potter";
-    string birthDate1 = "6/24/1992";
-    string workPhoneNumber1 = "work 000-000-0101";
-    string cellPhoneNumber1 = "cell 000-000-0189";
-    
-    Console.WriteLine ("" + name1);
-    Console.WriteLine("" +birthDate1);
-    Console.WriteLine("" + workPhoneNumber1);
-    Console.WriteLine("" + cellPhoneNumber1);
-    Console.WriteLine("");
+    string name = "Robert Hall";
+    string birthDate = "2/27/1949";
+    string workPhoneNumber = "work 000-444-0101";
+    string cellPhoneNumber = "cell 000-444-0189";
     
     
-
-    string name2= "John Smith";
-    string birthDate2 = "8/15/1978";
-    string workPhoneNumber2 = "work 000-111-0101";
-    string cellPhoneNumber2 = "cell 000-111-0189";
-    
-    Console.WriteLine ("" + name2);
-    Console.WriteLine("" +birthDate2);
-    Console.WriteLine("" + workPhoneNumber2);
-    Console.WriteLine("" + cellPhoneNumber2);
-    Console.WriteLine("");
-
-    string name3= "Chris Johnson";
-    string birthDate3 = "12/01/1987";
-    string workPhoneNumber3 = "work 000-222-0101";
-    string cellPhoneNumber3 = "cell 000-222-0189";
-    
-    Console.WriteLine ("" + name3);
-    Console.WriteLine("" +birthDate3);
-    Console.WriteLine("" + workPhoneNumber3);
-    Console.WriteLine("" + cellPhoneNumber3);
-    Console.WriteLine("");
-
-    string name4= "Robert Hall";
-    string birthDate4 = "2/27/1949";
-    string workPhoneNumber4 = "work 000-444-0101";
-    string cellPhoneNumber4 = "cell 000-444-0189";
-    
-    
-    Console.WriteLine ("" + name4);
-    Console.WriteLine("" +birthDate4);
-    Console.WriteLine("" + workPhoneNumber4);
-    Console.WriteLine("" + cellPhoneNumber4);
-    Console.WriteLine("");
-    
+    Console.WriteLine ("" + name);
+    Console.WriteLine("" + birthDate);
+    Console.WriteLine("" + workPhoneNumber);
+    Console.WriteLine("" + cellPhoneNumber);
+   
   }
 }
